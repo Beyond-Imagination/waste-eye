01663b1f8485f8aabc22be917eb908d04bd70731
eggplantiny <eggplantiny@gmail.com>
2021-07-30 03:34:29 +0900

    [https://beyond-imagination.jetbrains.space/p/wasteye/issues/28] 지도 Marker 구현

--- layouts/FullScreen.vue
+++ layouts/FullScreen.vue
@@ -6,7 +6,7 @@
 
 <script>
 export default {
-  name: 'map'
+  name: 'FullScreen'
 }
 </script>
 
--- pages/index.vue
+++ pages/index.vue
@@ -1,17 +1,32 @@
 <template>
   <client-only>
     <naver-maps
-      ref="map"
+      ref="maps"
       :map-options="mapOptions"
       class="full-page"
-    />
+      @load="onLoad"
+      @click="onClickMap"
+    >
+      <template v-for="(item, index) in items">
+        <naver-marker
+          :key="`${index}-marker`"
+          :lat="item.coordinates[1]"
+          :lng="item.coordinates[0]"
+          @click="onClickMarker(item)"
+        />
+      </template>
+    </naver-maps>
   </client-only>
 </template>
 
 <script lang="ts">
 import {
-  defineComponent, ref, onMounted
+  ref,
+  useContext,
+  defineComponent
 } from '@nuxtjs/composition-api'
+
+import { API } from '~/types'
 
 function getGeolocation (): Promise<GeolocationPosition> {
   return new Promise((resolve, reject) => navigator.geolocation.getCurrentPosition(resolve, reject))
@@ -19,30 +34,60 @@ function getGeolocation (): Promise<GeolocationPosition> {
 
 export default defineComponent({
   name: 'Index',
-  layout: 'map',
+  layout: 'FullScreen',
   setup () {
-    const map = ref()
+    const context = useContext()
+    const maps = ref()
     const mapOptions = ref({
-      lat: 37,
-      lng: 127,
-      zoom: 15,
-      zoomControl: true,
+      lat: 37.564214,
+      lng: 127.001699,
+      zoom: 14,
+      zoomControl: false,
       zoomControlOptions: { position: 'TOP_LEFT' }
     })
+    const items = ref<API.Trash[]>([])
 
-    const setCenter = (lat, lng) => map.value.setCenter(new naver.maps.LatLng(lat, lng))
+    // @ts-ignore
+    const setCenter = (lat: number, lng: number) => maps.value.setCenter(new naver.maps.LatLng(lat, lng), true)
+    const setZoomLevel = (level: number) => maps.value.setZoom(level, false)
 
-    onMounted(async () => {
+    const updateItem = async (lat: number, lng: number) => {
+      const { result } = await context.$axios.$get(`/api/trashes/${lat}/${lng}?radius=3000`)
+      items.value = result
+    }
+
+    const onLoad = async () => {
       const geo = await getGeolocation()
-      mapOptions.value.lng = geo.coords.longitude
-      mapOptions.value.lat = geo.coords.latitude
+      const { latitude, longitude } = geo.coords
+      await updateCoordinate(latitude, longitude)
+    }
 
-      setCenter(geo.coords.latitude, geo.coords.longitude)
-    })
+    const updateCoordinate = async (lat: number, lng: number) => {
+      mapOptions.value.lat = lat
+      mapOptions.value.lng = lng
+
+      setCenter(lat, lng)
+      await updateItem(lat, lng)
+    }
+
+    const onClickMarker = (item: API.Trash) => {
+      setCenter(item.coordinates[1], item.coordinates[0])
+      setZoomLevel(17)
+    }
+
+    const onClickMap = (item: any) => {
+      const lat = item.coord.y
+      const lng = item.coord.x
+      updateCoordinate(lat, lng)
+    }
 
     return {
-      map,
-      mapOptions
+      items,
+      maps,
+      mapOptions,
+      onLoad,
+      onClickMarker,
+      onClickMap
     }
   }
 })
