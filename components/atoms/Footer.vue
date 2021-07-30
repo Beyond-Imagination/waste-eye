aee55f854ef13e501956898788d07da1e61e6320
eggplantiny <eggplantiny@gmail.com>
2021-07-30 18:05:48 +0900

    [https://beyond-imagination.jetbrains.space/p/wasteye/issues/34] 푸터 추가

---
+++ components/atoms/Footer.vue
@@ -0,0 +1,25 @@
+<template>
+  <v-footer
+    class="pa-4 footer justify-center mt-8"
+    color="white"
+  >
+    All rights reserved <a href="https://github.com/orgs/Beyond-Imagination/teams"> Beyond Imagination.</a> 2020. 7
+  </v-footer>
+</template>
+
+<script>
+export default {
+  name: 'Footer'
+}
+</script>
+
+<style scoped>
+a {
+  margin: 0 8px;
+  text-decoration: none;
+}
+
+.footer {
+  width: 100%;
+}
+</style>
--- layouts/default.vue
+++ layouts/default.vue
@@ -9,16 +9,19 @@
         <Nuxt />
       </v-container>
     </v-main>
+    <Footer />
   </v-app>
 </template>
 
 <script>
 import { defineComponent } from '@nuxtjs/composition-api'
 import AppBar from '~/components/atoms/AppBar'
+import Footer from '~/components/atoms/Footer'
 
 export default defineComponent({
   components: {
-    AppBar
+    AppBar,
+    Footer
   }
 })
 </script>
