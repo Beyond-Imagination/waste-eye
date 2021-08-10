c736fd8e7b04bd3083677653e00af9261a2f7d83
eggplantiny <eggplantiny@gmail.com>
2021-08-10 18:47:18 +0900

    update ecosystem.config

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -3,8 +3,8 @@ module.exports = {
     {
       name: 'Wasteye',
       exec_mode: 'cluster',
-      instances: 'max', // Or a number of instances
-      script: './node_modules/nuxt/bin/nuxt.js',
+      instances: '1', // Or a number of instances
+      script: 'node -r tsconfig-paths/register ./node_modules/nuxt/bin/nuxt.js start',
       args: 'start'
     }
   ]
